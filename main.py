fce51c1c8a91e0af368aa327254ceaf7565a2652
liam123kxnx <linafriday10@gmail.com>
2024-11-18 18:13:39 +0100

    made a string

---
+++ main.py
@@ -0,0 +1,18 @@
+swords = ["Longsword","katana","cutlass","jitte","saber","bisento","ace"]
+numbers = [1,2,3,4,5,6,7,]
+print(swords)
+print(swords[-1])
+swords.pop()
+print(swords)
+swords.sort()
+print(swords)
+swords.append
+print(swords)
+swords.count
+print(swords)
+
+
+
+
+
+
